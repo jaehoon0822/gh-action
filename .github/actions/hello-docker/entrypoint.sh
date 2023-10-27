fc5b1e29127ec0aae27cc31a339c4d3525b1dd34
JHoon <jaehoon0822@gmail.com>
2023-10-27 13:04:17 +0900

    cleanup.sh interprinter setting

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -5,5 +5,5 @@ TIME=$(date)
 echo "::debug::Running entrypoint.sh"
 echo "Hello $1"
 echo "HELLO: $HELLO"
-echo "time=$TIME" >> $GITHUG_OUTPUT
+echo "time=$TIME" >> $GITHUB_OUTPUT
 echo "HELLO_TIME=$TIME" >> $GITHUB_ENV
